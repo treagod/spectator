be44f7e4cc8d6eabd3b06eae1b8d228168b5492f
Marvin Ahlgrimm <marv.ahlgrimm@gmail.com>
2021-05-02 14:19:21 +0200

    Added carousel

--- docs/pages/docs/getting-started/configure_body.vue
+++ docs/pages/docs/getting-started/configure_body.vue
@@ -5,6 +5,12 @@
     </div>
 
     <nuxt-content id="doc-content" :document="configure_body" />
+
+    <div class="columns is-vcentered has-text-centered">
+      <div @click="previous " class="column is-narrow is-arrow">&lt;</div>
+      <div class="column"><img :src="currentImage"/></div>
+      <div @click="next" class="column is-narrow is-arrow">&gt;</div>
+    </div>
   </getting-started-base>
 </template>
 
@@ -16,6 +22,44 @@ export default Vue.extend({
   components: {
     GettingStartedBase: Base,
 
+  },
+  data() {
+    return {
+      imgIndex : 0,
+      imageSrcs: [
+        "create_request/create_request.png",
+        "create_request/create_request_dialog.png"
+      ]
+    }
+  },
+  methods: {
+    getImgIndex () : number {
+      return this.imgIndex;
+    },
+    getImgSrc(index : number) : string {
+      return this.imageSrcs[index];
+    },
+    next() {
+      let nextIdx = this.imgIndex + 1;
+      if (nextIdx >= this.imageSrcs.length) {
+        this.imgIndex = 0;
+      } else {
+        this.imgIndex = nextIdx;
+      }
+    },
+    previous() {
+      let previousIdx = this.imgIndex - 1;
+      if (previousIdx < 0) {
+        this.imgIndex = this.imageSrcs.length - 1;
+      } else {
+        this.imgIndex = previousIdx;
+      }
+    }
+  },
+  computed: {
+    currentImage() : string {
+      return this.getImgSrc(this.getImgIndex());
+    }
   },
   head() {
     return {
@@ -42,5 +86,12 @@ export default Vue.extend({
 <style>
 #doc-content .subtitle {
   margin-top: 0.75em;
+}
+
+.is-arrow {
+  font-weight: bold;
+  font-size: 2rem;
+  cursor: pointer;
+  color: #668e2c;
 }
 </style>
